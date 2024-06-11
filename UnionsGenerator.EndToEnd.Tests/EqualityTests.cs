9a23ed7fcd562c06713cf019ee5afd6045e0feea
Paul Brätz <paul.braetz@rhomicro.com>
2024-06-11 16:16:13 +0200

    add failing e2e test

---
+++ UnionsGenerator.EndToEnd.Tests/EqualityTests.cs
@@ -0,0 +1,16 @@
+﻿namespace RhoMicro.CodeAnalysis.UnionsGenerator.EndToEnd.Tests;
+
+using RhoMicro.CodeAnalysis;
+
+using System;
+
+public partial class EqualityTests
+{
+    [UnionType<Int32>]
+    partial class Foo
+    {
+        public Boolean Equals(Foo? foo) => true;
+    }
+    [UnionType<Int32>]
+    partial class Bar;
+}
