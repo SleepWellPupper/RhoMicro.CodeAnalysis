502b03810bf3e1338b2ef463677ff56334d76c2f
Paul Brätz <paul.braetz@rhomicro.com>
2024-07-15 22:48:32 +0200

    improve tests

--- UnionsGenerator.Tests/NullableTests.cs
+++ UnionsGenerator.Tests/NullableTests.cs
@@ -23,6 +23,13 @@ public class NullableTests : TestBase
     [Theory]
     [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsFoo", true, "AsFoo")]
     [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsBar", true, "AsBar")]
+    [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsFoo", false, "AsBar")]
+    [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsBar", false, "AsFoo")]
+
+    [InlineData(new[] { "Foo" }, new String[] { "Bar" }, "IsFoo", true, "AsFoo")]
+    [InlineData(new[] { "Foo" }, new String[] { "Bar" }, "IsBar", true)]
+    [InlineData(new[] { "Foo" }, new String[] { "Bar" }, "IsFoo", false)]
+    [InlineData(new[] { "Foo" }, new String[] { "Bar" }, "IsBar", false, "AsFoo")]
     public void AnnotatesWithMemberNotNullWhenAttribute(
         String[] classNames,
         String[] structNames,
@@ -34,10 +41,10 @@ public class NullableTests : TestBase
             $$"""
             using RhoMicro.CodeAnalysis;
 
-            {{String.Join('\n', classNames.Select(n => $"class {n} {{}}"))}}
+            {{String.Join('\n', classNames.Select(n => $"class {n.TrimEnd('?')} {{}}"))}}
             {{String.Join('\n', structNames.Select(n => $"struct {n} {{}}"))}}
 
-            [UnionType<{{String.Join(',', classNames.Concat(structNames))}}>]
+            [UnionType<{{String.Join(',', structNames.Concat(classNames.Select(n=>n.TrimEnd('?'))))}}>]
             partial struct Union { }
             """,
             s =>
@@ -48,23 +55,31 @@ public class NullableTests : TestBase
                     .GetAttributes()
                     .Where(a =>
                         a.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ==
-                        "global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen")
+                        "global::System.Diagnostics.CodeAnalysis.MemberNotNullWhenAttribute")
                     .Select(a => a.ConstructorArguments)
                     .Select(args =>
-                        args.Length == expectedMemberNames.Length + 1
-                        && args[0] is
-                        {
-                            Kind: TypedConstantKind.Primitive,
-                            Value: Boolean actualCondition
-                        }
-                        && actualCondition == expectedCondition
-                        && args.Skip(1)
-                            .Zip(expectedMemberNames)
-                            .All(t => t.First is
+                    {
+                        if(expectedMemberNames.Length == 0)
+                            return true;
+
+                        var result = args.Length == expectedMemberNames.Length + 1
+                            && args[0] is
                             {
                                 Kind: TypedConstantKind.Primitive,
-                                Value: String actualMemberName
-                            } && actualMemberName == t.Second))
+                                Value: Boolean actualCondition
+                            }
+                            && actualCondition == expectedCondition
+                            && args.Skip(1)
+                                .Zip(expectedMemberNames)
+                                .All(t => t.First is
+                                {
+                                    Kind: TypedConstantKind.Primitive,
+                                    Value: String actualMemberName
+                                } && actualMemberName == t.Second);
+
+                        return result;
+                    })
+                    .Where(v => v)
                     .SingleOrDefault();
 
                 Assert.True(hasMatchingAttribute);
