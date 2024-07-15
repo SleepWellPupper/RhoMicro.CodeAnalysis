4d280f97291bdda342134bdddf58dd72d6d73445
Paul Brätz <paul.braetz@rhomicro.com>
2024-07-15 21:47:15 +0200

    add failing test

--- UnionsGenerator.Tests/NullableTests.cs
+++ UnionsGenerator.Tests/NullableTests.cs
@@ -1,9 +1,8 @@
 ﻿namespace RhoMicro.CodeAnalysis.UnionsGenerator.Tests;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
 
 public class NullableTests : TestBase
 {
@@ -19,5 +18,57 @@ public class NullableTests : TestBase
             partial struct NullableBoolUnion { }
             """,
             s => { });
+    }
+
+    [Theory]
+    [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsFoo", true, "AsFoo")]
+    [InlineData(new[] { "Foo", "Bar" }, new String[] { }, "IsBar", true, "AsBar")]
+    public void AnnotatesWithMemberNotNullWhenAttribute(
+        String[] classNames,
+        String[] structNames,
+        String propertyName,
+        Boolean expectedCondition,
+        params String[] expectedMemberNames)
+    {
+        TestUnionType(
+            $$"""
+            using RhoMicro.CodeAnalysis;
+
+            {{String.Join('\n', classNames.Select(n => $"class {n} {{}}"))}}
+            {{String.Join('\n', structNames.Select(n => $"struct {n} {{}}"))}}
+
+            [UnionType<{{String.Join(',', classNames.Concat(structNames))}}>]
+            partial struct Union { }
+            """,
+            s =>
+            {
+                var hasMatchingAttribute = s.GetMembers()
+                    .OfType<IPropertySymbol>()
+                    .Single(p => p.Name == propertyName)
+                    .GetAttributes()
+                    .Where(a =>
+                        a.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ==
+                        "global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen")
+                    .Select(a => a.ConstructorArguments)
+                    .Select(args =>
+                        args.Length == expectedMemberNames.Length + 1
+                        && args[0] is
+                        {
+                            Kind: TypedConstantKind.Primitive,
+                            Value: Boolean actualCondition
+                        }
+                        && actualCondition == expectedCondition
+                        && args.Skip(1)
+                            .Zip(expectedMemberNames)
+                            .All(t => t.First is
+                            {
+                                Kind: TypedConstantKind.Primitive,
+                                Value: String actualMemberName
+                            } && actualMemberName == t.Second))
+                    .SingleOrDefault();
+
+                Assert.True(hasMatchingAttribute);
+            },
+            "Union");
     }
 }
