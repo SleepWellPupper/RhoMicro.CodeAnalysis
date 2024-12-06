39b74aa70f12a0a62894f78580a0c7b195e4e322
Paul Brätz <paul.braetz@rhomicro.com>
2024-12-06 16:15:20 +0100

    clean test app

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,3 @@
-using RhoMicro.CodeAnalysis;
-
 internal class Program
 {
     private static void Main(String[] _0)
@@ -7,16 +5,3 @@ internal class Program
 
     }
 }
-
-[UnionType<ErrorCode, MultipleUsersError, User>(Storage = StorageOption.Value)]
-readonly partial struct GetUserResult;
-
-sealed record User(String Name);
-
-enum ErrorCode
-{
-    NotFound,
-    Unauthorized
-}
-
-readonly record struct MultipleUsersError(Int32 Count);
--- TestApp/SettingsNamespace/NamespaceSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace TestApp;
-
-using RhoMicro.CodeAnalysis;
-
-[JsonSchema]
-sealed partial class NamespaceSettings
-{
-    public Double NumberProp { get; set; }
-}
