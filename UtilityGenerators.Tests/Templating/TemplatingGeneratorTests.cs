9b6750158c9e881ab559b056fa52687f4bbe2807
Paul Brätz <paul.braetz@rhomicro.com>
2025-03-02 14:51:34 +0100

    increase timeout (compensate for slow cicd vm)

--- UtilityGenerators.Tests/Templating/TemplatingGeneratorTests.cs
+++ UtilityGenerators.Tests/Templating/TemplatingGeneratorTests.cs
@@ -354,7 +354,7 @@ public class TemplatingGeneratorTests : TestBase<TemplatingGenerator>
         """")]
     public void CompilesTemplate(String name, String template)
     {
-        using var cts = new CancellationTokenSource(Debugger.IsAttached ? -1 : 2500);
+        using var cts = new CancellationTokenSource(Debugger.IsAttached ? -1 : 10000);
 
         try
         {
