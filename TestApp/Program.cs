5f6990ee515f0296f7b8e70f2217ff45b4f3ed89
Paul Brätz <paul.braetz@rhomicro.com>
2025-03-02 15:39:19 +0100

    empty out testapp

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,10 +1,3 @@
-using System.Diagnostics.Tracing;
-using System.Text.Json.Nodes;
-using System.Text.RegularExpressions;
-
-using RhoMicro.CodeAnalysis.Library.Models;
-using RhoMicro.CodeAnalysis.Templating;
-
 internal partial class Program
 {
     [RhoMicro.CodeAnalysis.NonEquatable]
@@ -12,18 +5,5 @@ internal partial class Program
 
     private static void Main(String[] _0)
     {
-        using var ctx = ModelCreationContext.CreateDefault(CancellationToken.None);
-        var template = TemplateSyntaxModel.Parse(
-"""
-// \§(escaped hole)
-// \\§(Name) (unescaped hole)
-public §(Accessibility) class §(Name)
-{§{
-    foreach(var member in Members)
-    {
-        §(member)
-    }
-}}
-""", in ctx);
     }
 }
