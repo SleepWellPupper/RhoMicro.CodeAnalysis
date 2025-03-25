84e078df8b67a58d48c25bd29f0545a12c3ad649
Paul Brätz <paul.braetz@rhomicro.com>
2025-03-24 23:34:08 -0400

    add tests

--- UtilityGenerators.Tests.E2E/Library/Text/Templating/NamedTypeTemplateTests.cs
+++ UtilityGenerators.Tests.E2E/Library/Text/Templating/NamedTypeTemplateTests.cs
@@ -13,6 +13,98 @@ using RhoMicro.CodeAnalysis.Library.Text.Templating;
 public partial class NamedTypeTemplateTests
 {
     [Fact]
+    public void RendersBaseList()
+    {
+        var actual = new NamedTypeTemplate(new NamedTypeModel(
+            ContainingTypes: [],
+            Kind: PartialTypeKindModel.Class,
+            TypeArguments: [],
+            NamespaceParts: ["RhoMicro", "Test"],
+            Name: "Foo"),
+            baseList: ["Bar", "IBaz"],
+            comment: DocsCommentTemplate.Create("")).ToString();
+
+        Assert.Equal(
+            """
+            namespace RhoMicro.Test
+            {
+                partial class Foo : Bar, IBaz;
+            }
+            """, actual);
+    }
+    [Fact]
+    public void RendersSinglelineSummaryComment()
+    {
+        var actual = new NamedTypeTemplate(new NamedTypeModel(
+            ContainingTypes: [],
+            Kind: PartialTypeKindModel.Class,
+            TypeArguments: [],
+            NamespaceParts: ["RhoMicro", "Test"],
+            Name: "Foo"),
+            baseList: [],
+            comment: DocsCommentTemplate.Create("FooBar")).ToString();
+
+        Assert.Equal(
+            """
+            namespace RhoMicro.Test
+            {
+                /// <summary>
+                /// FooBar
+                /// </summary>
+                partial class Foo;
+            }
+            """, actual);
+    }
+    [Fact]
+    public void RendersMultilineSummaryComment()
+    {
+        var actual = new NamedTypeTemplate(new NamedTypeModel(
+            ContainingTypes: [],
+            Kind: PartialTypeKindModel.Class,
+            TypeArguments: [],
+            NamespaceParts: ["RhoMicro", "Test"],
+            Name: "Foo"),
+            baseList: [],
+            comment: DocsCommentTemplate.Create("Foo\nBar\nBaz")).ToString();
+
+        Assert.Equal(
+            """
+            namespace RhoMicro.Test
+            {
+                /// <summary>
+                /// Foo
+                /// Bar
+                /// Baz
+                /// </summary>
+                partial class Foo;
+            }
+            """, actual);
+    }
+    [Template("(:text:)")]
+    readonly partial struct DisplayTemplate(String text);
+    [Fact]
+    public void RendersTypeIndentedInNamespace()
+    {
+        var actual = new NamedTypeTemplate(new NamedTypeModel(
+            ContainingTypes: [],
+            Kind: PartialTypeKindModel.Class,
+            TypeArguments: [],
+            NamespaceParts: ["RhoMicro", "Test"],
+            Name: "Foo")).RenderToString(
+                body: new DisplayTemplate("public static int Main() => 0;"));
+
+        Assert.Equal(
+            """
+            namespace RhoMicro.Test
+            {
+                partial class Foo
+                {
+                    public static int Main() => 0;
+                }
+            }
+            """, actual);
+    }
+    [Fact]
     public void RendersBlockScopedNamespace()
     {
         var actual = new NamedTypeTemplate(new NamedTypeModel(
