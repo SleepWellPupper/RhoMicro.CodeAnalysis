3e587678c441a8a677bb9300ad8e84f8df486ca1
Paul Brätz <paul.braetz@rhomicro.com>
2025-03-24 23:06:59 -0400

    add docs comment template

---
+++ UtilityGenerators/Library/Text/Templating/DocsCommentTemplate.cs
@@ -0,0 +1,25 @@
+﻿namespace RhoMicro.CodeAnalysis.Library.Text.Templating;
+
+[Template(
+    """
+    {:
+        if(_summary is null or [])
+            return;
+    :}
+    (:DocumentationComment, _summary:)
+
+    """)]
+
+#if RHOMICRO_CODEANALYSIS_UTILITYGENERATORS
+[IncludeFile]
+#endif
+#if GENERATOR
+[NonEquatable]
+#endif
+internal readonly partial struct DocsCommentTemplate
+{
+    private DocsCommentTemplate(String summary) => _summary = summary;
+    private readonly String _summary;
+
+    public static DocsCommentTemplate Create(String summary) => new(summary);
+}
