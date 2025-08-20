599bd747cc52b156accb75929dd016f85765d386
SleepWellPupper <95855091+SleepWellPupper@users.noreply.github.com>
2025-08-20 02:58:16 +0200

    Fix type arrays (#149)
    * remove rewriter remainder
    
    * fix type arrays
    
    ---------
    
    Co-authored-by: Paul Braetz <paul.braetz@rhomicro.com>

---
+++ UtilityGenerators.Tests.E2E/TestAttribute.cs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MPL-2.0
+
+namespace RhoMicro.CodeAnalysis.Tests.E2E;
+
+[GenerateFactory]
+partial class TestAttribute
+{
+    public Type[] TypeArrayProperty { get; set; } = [];
+    public Type?[] NullableTypeArrayProperty { get; set; } = [];
+    public Type[]? TypeNullableArrayProperty { get; set; } = [];
+    public Type?[]? NullableTypeNullableArrayProperty { get; set; } = [];
+}
