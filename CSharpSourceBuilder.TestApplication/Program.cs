f52ac7c9ddb64f04909f6c9456cab3f4257e7a5c
Paul Braetz <paul.braetz@rhomicro.com>
2025-12-09 03:14:35 +0100

    utilize lyra

--- CSharpSourceBuilder.TestApplication/Program.cs
+++ CSharpSourceBuilder.TestApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using RhoMicro.CodeAnalysis;
+using RhoMicro.CodeAnalysis.Lyra;
 
 using var builder = new CSharpSourceBuilder();
 
