8d8805e783ec153639f495934f5a793272b7a412
Paul Braetz <paul.braetz@rhomicro.com>
2025-12-21 19:34:54 +0100

    add named factory methods for overload ambiguity resolution

--- Janus.Analyzers/Components/FactoriesComponent.cs
+++ Janus.Analyzers/Components/FactoriesComponent.cs
@@ -201,6 +201,28 @@ internal readonly record struct FactoriesComponent(UnionModel Model) : ICSharpSo
                       {
                           ct.ThrowIfCancellationRequested();
 
+                          b.Append($"Creates an instance of {Cref(m.DocsCommentId)}.");
+                      })}}
+                      {{Param("value", m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append($"The value to create an instance of {Cref(m.DocsCommentId)} from.");
+                      })}}
+                      {{Returns(m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append($"The new instance of {Cref(m.DocsCommentId)}.");
+                      })}}
+                      public static {{new UnionTypeNameComponent(m)}} CreateFrom{{variant.Name}}(
+                          {{variant.Type.NullableName}} value)
+                          => new {{new UnionTypeNameComponent(m)}}(value, validate: true);
+                          
+                      {{Summary(m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
                           b.Append($"Attempts to create an instance of {Cref(m.DocsCommentId)}.");
                       })}}
                       {{Param("value", m, static (m, b, ct) =>
@@ -226,6 +248,48 @@ internal readonly record struct FactoriesComponent(UnionModel Model) : ICSharpSo
                           b.Append($"{Langword("true")} if an instance of {Cref(m.DocsCommentId)} could be created; otherwise, {Langword("false")}.");
                       })}}
                       public static bool TryCreate(
+                          {{variant.Type.NullableName}} value,
+                          [{{typeof(NotNullWhenAttribute)}}(true)]
+                          out {{new UnionTypeNameComponent(m, RenderNullable: true)}} union)
+                      {
+                          var isValid = true;
+                          Validate(value, throwIfInvalid: false, ref isValid);
+                          union = isValid 
+                              ? new {{new UnionTypeNameComponent(m)}}(value, validate: false) 
+                              : default;
+                              
+                          return isValid;
+                      }
+
+                      {{Summary(m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append($"Attempts to create an instance of {Cref(m.DocsCommentId)}.");
+                      })}}
+                      {{Param("value", m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append($"The value to create an instance of {Cref(m.DocsCommentId)} from.");
+                      })}}
+                      {{Param("union", m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append(
+                              $"""
+                               The instance of {Cref(m.DocsCommentId)} if one could be created; otherwise, 
+                               {(m.TypeKind is UnionTypeKind.Class ? Langword("null") : Langword("default"))}.
+                               """);
+                      })}}
+                      {{Returns(m, static (m, b, ct) =>
+                      {
+                          ct.ThrowIfCancellationRequested();
+
+                          b.Append($"{Langword("true")} if an instance of {Cref(m.DocsCommentId)} could be created; otherwise, {Langword("false")}.");
+                      })}}
+                      public static bool TryCreateFrom{{variant.Name}}(
                           {{variant.Type.NullableName}} value,
                           [{{typeof(NotNullWhenAttribute)}}(true)]
                           out {{new UnionTypeNameComponent(m, RenderNullable: true)}} union)
